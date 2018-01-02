3808379c8eb3ac60951938aff1d2dd42cf6932a9
redjoy <149825990@qq.com>
2018-01-02 22:17:05 +0800

    use redux

--- src/App.js
+++ src/App.js
@@ -5,6 +5,40 @@ import logo from './logo.svg';
 import './App.css';
 import {createStore} from 'redux';
 
+
+// redux
+/*
+  1.新建store
+  通过reducer建立
+  根据旧的state和action 生成新的 state
+*/
+function counter(state=0,action){
+  switch(action.type){
+    case '生宝宝':
+      return state+1;
+    case '不要宝宝了':
+      return state-1;
+    default:
+      return 10;
+  }
+}
+
+// 新建store
+const store = createStore(counter);
+
+const init = store.getState();
+console.log(init);
+function listener() {
+  const current = store.getState();
+  console.log(`现在有宝宝${current}个`);
+}
+// 订阅事件
+store.subscribe(listener);
+
+// 派发事件
+store.dispatch({type:'生宝宝'});
+console.log(store.getState());
+
 class App extends Component {
   render() {
     const title = 'learn'
