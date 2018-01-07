8f35db5ea238a78b3b1698d1168c7c5ae035ff64
redjoy <149825990@qq.com>
2018-01-07 18:09:49 +0800

    配置proxy关联不同端口间 MongoDB的调用

--- src/Auth.js
+++ src/Auth.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {login} from './Auth.redux';
 import {Redirect} from 'react-router-dom';
+import axios from 'axios';
 
 // 两个reducers 每个reducers 都有一个state
 @connect(
@@ -10,12 +11,24 @@ import {Redirect} from 'react-router-dom';
 )
 
 class Auth extends Component {
-	constructor(props){
-		super(props)
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {}
+		}
+	}
+	componentDidMount() {
+		axios.get('/data')
+			.then(res =>{
+				if(res.status == 200) {
+					this.setState({data:res.data})
+				}
+			})
 	}
 	render(){
 		return (
 			<div>
+				<h2>我的名字是{this.state.data.user}</h2>
 				{this.props.isAuth ? <Redirect to='/dashboard'/> : null}
 				<h2>你没有权限，需要登录才能访问</h2>
 				<button onClick={this.props.login}>登录</button>
